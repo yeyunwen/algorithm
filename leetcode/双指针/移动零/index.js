1d7ea284e9ff1da51d68168f3b58cbf88c8aa4d3
yeyunwen <1571374338@qq.com>
2024-08-12 11:23:57 +0800

    feat: 283.移动零

---
+++ leetcode/双指针/移动零/index.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  let j = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+      j++;
+    }
+  }
+};
+
+// 思路：双指针
+
+// 时间复杂度：O(n)
+// 指针i不断向右移动，遇到非0就和j交换，j再往前移动
+// 这样所有非0元素都被移到了前面
+// 空间复杂度：O(1)
+
+// 测试用例
+const nums1 = [0, 1, 0, 3, 12];
+moveZeroes(nums1);
+console.log(nums1); // [1, 3, 12, 0, 0]
